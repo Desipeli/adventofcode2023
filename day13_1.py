aab221c894dee8611ff21b305203c6f03267fe8a
desipeli <mr.desipeli@gmail.com>
2023-12-13 17:00:48 +0200

    part1

---
+++ day13_1.py
@@ -0,0 +1,52 @@
+from utils.input import get_input_list
+
+patterns_input = get_input_list(13)
+blocks = []
+block = []
+
+for line in patterns_input:
+    if line == "":
+        blocks.append(block.copy())
+        print("saved", block)
+        block.clear()
+        continue
+    block.append(line)
+blocks.append(block)
+
+def find_reflections(block :list, istart: int = 0, iplus: int = 0):
+    i1 = istart - iplus
+    i2 = istart + iplus + 1
+    if i1 == len(block)-1: # Ei voida aloittaa viimeiseltä riviltä
+        return False
+    if i1 < 0 or i2 >= len(block): # Ollaan menty rajan yli, eli löytyi
+        return True
+    if not block[i1] == block[i2]:
+        return False
+    return find_reflections(block, istart, iplus+1)
+
+def flip_block(block):
+    flipped = []
+    for x in range(len(block[0])):
+        column = [i[x] for i in block]
+        flipped.append("".join(column))
+    return flipped
+
+
+total_sum = 0
+
+for block in blocks:
+    found = False
+    for i in range(len(block)):
+        if find_reflections(block, i):
+            found = True
+            break
+    if found:
+        total_sum += (i+1)*100
+    else:
+        flipped_block = flip_block(block)
+        for i in range(len(flipped_block)):
+            if find_reflections(flipped_block, i):
+                total_sum += i+1
+                break
+
+print(total_sum)
--- utils/input.py
+++ utils/input.py
@@ -5,3 +5,11 @@ def get_input_list(day: int, variant: int = 0) -> list:
     with open(path, "r") as file:
         data = [line.rstrip("\n") for line in file.readlines()]
     return data
+
+def get_input_string(day: int, variant: int = 0) -> list:
+    path = f"inputs/day{day}.txt"
+    if variant:
+        path = f"inputs/day{day}_{variant}.txt"
+    with open(path, "r") as file:
+        data = [x.rstrip("\n") for x in file.readlines()]
+    return data
